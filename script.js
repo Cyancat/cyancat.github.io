cf7fc55968359ea4e7a6e3be808fbc89fa7ba373
Cyancat <aycnight@gmail.com>
2018-02-11 15:46:38 +0800

    Add available value filter

--- script.js
+++ script.js
@@ -29,10 +29,43 @@ function flatten(json){
     if (typeof json[obj] === 'object') {
       // console.log(obj);
       for (var obj_c in json[obj]) {
-        json[obj + '_' + obj_c] = json[obj][obj_c];
+        if (vali_var(obj_c)) {
+          json[obj + '_' + obj_c] = json[obj][obj_c];
+        }
       }
       delete json[obj];
     }
   }
   return json;
+}
+
+function vali_var(v) {
+  var vali = [
+    // quality
+    'img_count',
+    'attr_key_count',
+    'source_count',
+    'has_title_cn',
+    'comments_count_jp',
+    'product_click_count',
+    'score_jp',
+    'sales',
+    'has_ean',
+    // decay
+    'is_released',
+    'release_date',
+    'create_date',
+    // stock
+    'has_mall_spot',
+    'has_agent_new',
+    'has_mall_presell',
+    'has_amazon',
+    'has_surugaya'
+  ], result = false;
+  vali.forEach(function(e){
+    if ( e == v ){
+      result = true;
+    }
+  });
+  return result;
 }
