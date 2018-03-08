43382b46efb4abd07e3cd65df8e3d5b74e4501bc
Cyancat <aycnight@gmail.com>
2018-03-08 23:14:54 +0800

    Remove quote mark in text fields

--- script.js
+++ script.js
@@ -2,15 +2,32 @@
 
 $(function(){
   $("#convert").click(function() {
-    var json = JSON.parse(
-      $("#target").val()
-        /* May be postman */
-        .replace(/\\\\"/g, '"')
-        .replace(/\\\\u[\da-z]{0,3}"/g, '"')
-        /* 8200/_plugin/head/ */
-        .replace(/\\"/g, '"')
-        .replace(/\\u[\da-z]{0,3}"/g, '"')
-    );
+
+    // 1. Replace incompleted unicode character
+    var json_prep =
+        $("#target").val()
+          /* May be postman */
+          .replace(/\\\\"/g, '"')
+          .replace(/\\\\u[\da-z]{0,3}"/g, '"')
+          /* 8200/_plugin/head/ */
+          .replace(/\\"/g, '"')
+          .replace(/\\u[\da-z]{0,3}"/g, '"');
+
+    // 2. Filter out all content quoted from :" and end by ",
+    var cq_s = json_prep.match(/:\s?"(.+?)",/g);
+
+    // 3. Replace all " between :" and ",
+    cq_s.forEach(function(c, i){
+      // 3.1 Get content between :" and ",
+      var ct = c.replace(/:\s?"(.+)",/g, '$1');
+
+      // 3.2 If the content contain ", then start replacing the content within origin JSON string
+      if (/"/.test(ct) == true) {
+        json_prep = json_prep.replace(ct, ct.replace(/"/g, ""));
+      }
+    })
+
+    var json = JSON.parse(json_prep);
 
     // Specially deal with the ES's result
     // console.log(json.hits instanceof Array);
@@ -47,7 +64,7 @@ function flatten(json){
         if (obj_c.indexOf('date') != -1) {
           json[obj + '_' + obj_c] = moment.unix(json[obj][obj_c]).format('Y-MM-DD') + '(' + json[obj][obj_c] + ')'
         }
-        
+
       }
       delete json[obj];
     }
